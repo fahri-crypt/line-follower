d3acb1232b38650b5f7529772fae7d0f7c7aae7b
fahri-crypt <fahrialia123@gmail.com>
2023-11-27 10:51:37 +0700

    Perubahan menggunakan driver l293d dengan motor shield hw130

--- line_follower_v1/line_follower_v1.ino
+++ line_follower_v1/line_follower_v1.ino
@@ -1,19 +1,13 @@
-#include <Arduino.h>
+#include <AFMotor.h>
+
+AF_DCMotor motorA(3);  // Motor connected to M1 port on HW130 motor shield
+AF_DCMotor motorB(4);  // Motor connected to M2 port on HW130 motor shield
 
 const unsigned int Emiter = 22;
 const unsigned int lines[] = {24, 26, 28, 30, 32, 34, 36, 38};
 const unsigned int numLines = sizeof(lines) / sizeof(lines[0]);
 
-const uint8_t maxSpeedA = 150;
-const uint8_t maxSpeedB = 150;
-const uint8_t baseSpeedA = 100;
-const uint8_t baseSpeedB = 100;
-
 int modePin = 8;
-int bPhasePin = 6; // kanan
-int bEnblPin = 7;
-int aPhasePin = 4; // kiri
-int aEnblPin = 5;
 
 void setup() {
   Serial.begin(9600);
@@ -25,10 +19,6 @@ void setup() {
   }
 
   pinMode(modePin, OUTPUT);
-  pinMode(aPhasePin, OUTPUT);
-  pinMode(aEnblPin, OUTPUT);
-  pinMode(bPhasePin, OUTPUT);
-  pinMode(bEnblPin, OUTPUT);
 }
 
 void loop() {
@@ -49,58 +39,30 @@ void loop() {
 }
 
 void lineFollower(int sensorValues[]) {
-  // Pengaturan kecepatan motor
-  int motorSpeedA, motorSpeedB;
-  int baseSpeed = (baseSpeedA + baseSpeedB) / 2;
-  int speedShift = 20; // Amount to reduce speed for each shift
-
   // Logika kontrol motor berdasarkan pembacaan sensor
   if (sensorValues[3] == HIGH && sensorValues[4] == HIGH) {
     // Jika kedua sensor tengah terdeteksi, maju lurus
-    motorSpeedA = motorSpeedB = baseSpeed;
+    motorA.setSpeed(100);
+    motorB.setSpeed(100);
+    motorA.run(FORWARD);
+    motorB.run(FORWARD);
   } else if (sensorValues[7] == HIGH) {
     // Jika sensor paling kiri terdeteksi, belok ke kiri
-    motorSpeedA = 0;
-    motorSpeedB = maxSpeedB - speedShift;
+    motorA.setSpeed(0);
+    motorB.setSpeed(100);
+    motorA.run(RELEASE);
+    motorB.run(FORWARD);
   } else if (sensorValues[6] == HIGH) {
     // Jika sensor sebelah kiri sedikit terdeteksi, belok sedikit ke kiri
-    motorSpeedA = baseSpeedA;
-    motorSpeedB = maxSpeedB - speedShift;
-  } else if (sensorValues[5] == HIGH) {
-    // Jika sensor sebelah kiri lebih sedikit terdeteksi, belok lebih sedikit ke kiri
-    motorSpeedA = maxSpeedA - speedShift;
-    motorSpeedB = baseSpeedB;
-  } else if (sensorValues[2] == HIGH) {
-    // Jika sensor sebelah kanan lebih sedikit terdeteksi, belok lebih sedikit ke kanan
-    motorSpeedA = baseSpeedA;
-    motorSpeedB = maxSpeedB - speedShift;
-  } else if (sensorValues[1] == HIGH) {
-    // Jika sensor sebelah kanan sedikit terdeteksi, belok sedikit ke kanan
-    motorSpeedA = maxSpeedA - speedShift;
-    motorSpeedB = baseSpeedB;
-  } else if (sensorValues[0] == HIGH) {
-    // Jika sensor paling kanan terdeteksi, belok ke kanan
-    motorSpeedA = maxSpeedA - speedShift;
-    motorSpeedB = 0;
-  } else {
-    // Jika tidak ada sensor yang terdeteksi, berhenti
-    motorSpeedA = motorSpeedB = 0;
-  }
+    motorA.setSpeed(100);
+    motorB.setSpeed(80);
+    motorA.run(FORWARD);
+    motorB.run(FORWARD);
+  } // Add more conditions as needed
 
   // Print kecepatan motor ke Serial Monitor
   Serial.print("Motor Speed A: ");
-  Serial.print(motorSpeedA);
+  // Serial.print(motorA.readSpeed());
   Serial.print(", Motor Speed B: ");
-  Serial.println(motorSpeedB);
-
-  // Set kecepatan motor menggunakan driver motor DRV8835
-  analogWrite(aEnblPin, motorSpeedA);
-  analogWrite(bEnblPin, motorSpeedB);
-
-  // Set arah putaran motor menggunakan fase (phase) pada DRV8835
-  digitalWrite(aPhasePin, (motorSpeedA > 0) ? HIGH : LOW);
-  digitalWrite(bPhasePin, (motorSpeedB > 0) ? HIGH : LOW);
-
-  // Set mode driver motor (mode) untuk kontrol kecepatan
-  digitalWrite(modePin, HIGH);
+  // Serial.println(motorB.readSpeed());
 }
